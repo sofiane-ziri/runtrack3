002a8ebd641fafe81b27c339c942efe7f0ae3ee1
sofiane-ziri <91732738+sofiane-ziri@users.noreply.github.com>
2022-03-14 11:02:50 +0100

    Jour01

---
+++ job04/script.js
@@ -0,0 +1,17 @@
+function bisextile(annee){
+
+    var i = annee % 4;
+    if(i==0)
+        i = annee % 100;
+    else
+        return false
+    if(i==0)
+        i = annee % 400;
+    else
+        return false
+    if(i==0)
+        return true;
+    else
+        return false
+}
+console.log(bisextile(2000));
