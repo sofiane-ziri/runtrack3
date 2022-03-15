89fa82997bb989e5409de6f53191a626f2b7e38b
sofiane-ziri <91732738+sofiane-ziri@users.noreply.github.com>
2022-03-15 12:34:30 +0100

    job01

---
+++ jour02/job01/script.js
@@ -0,0 +1,20 @@
+function citation(){
+   
+   
+    let btn = document.getElementById("button")
+    
+        let citation = document.getElementById("citation").textContent
+    
+    btn.addEventListener("click", () =>{
+
+        
+        console.log(citation)
+
+    });
+    
+}
+
+
+citation()
+
+
---
+++ jour02/job02/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>job02</title>
+
+</head>
+
+<body>
+    <article id="citation">
+        "La vie a beaucoup plus d’imagination que nous”.
+    </article>
+    <button id="button">click</button>
+    <script src="script.js"></script>
+</body>
+
+</html>
