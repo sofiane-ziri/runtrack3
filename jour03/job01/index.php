6cf92cdfc1352830c2c3624b1e0ab35c5fa288ba
sofiane-ziri <91732738+sofiane-ziri@users.noreply.github.com>
2022-03-16 11:46:06 +0100

    Job01

---
+++ jour03/job01/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script type="text/javascript" src="script.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <title>Jour 03</title>
+</head>
+
+<body>
+    <main>
+        <p>“le $ est un bon élément
+            monétaire. Le # de twitter est une bonne I D. Il faudra faire le point avec la
+            classe pour cacher cet élément. ”.</p>
+        <button class="btn1">Hide</button>
+        <button class="btn2">Show</button>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+
+</html>
---
+++ jour03/job01/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".btn1").click(function(){
+      $("body").hide();
+    });
+    $(".btn2").click(function(){
+      $("p").show();
+    });
+  });
