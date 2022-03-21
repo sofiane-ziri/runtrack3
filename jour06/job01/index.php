2e6de49b4d1acbcb089e6545954b0089ddcab386
sofiane-ziri <91732738+sofiane-ziri@users.noreply.github.com>
2022-03-21 13:23:43 +0100

    jour06

---
+++ jour06/job01/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style1.css">
+    <link rel="stylesheet" href="style2.css">
+    <link rel="stylesheet" href="style3.css">
+    <link rel="stylesheet" href="style4.css">
+    <title>Jour06</title>
+</head>
+<body>
+</body>
+</html>
---
+++ jour06/job03/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Jour06</title>
+</head>
+
+<body>
+    <section class="section"> </section>
+    <section class="section"> </section>
+    <section class="section"> </section>
+    <section class="section"></section>
+</body>
+
+</html>
